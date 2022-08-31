e94b411683d0f4967912abe2abb301901e467b2c
ItuMilla <itumelenglephondo94@gmail.com>
2022-08-31 15:51:40 +0200

    igit commit -m "price-plan"

--- index.js
+++ index.js
@@ -4,15 +4,57 @@ import express from 'express';
 
 const app = express();
 
-const db = await sqlite.open({
-    filename: './data_plan.do',
-    driver: sqlite3.Database
+// make re.body work
+app.use(express.json());
+
+const  db = await  sqlite.open({
+    filename:  './data_plan.db',
+    driver:  sqlite3.Database
 });
 
-console.log('do initialized');
+console.log('db initialized');
 
 
 await db.migrate();
+
+app.post('/api/price_pla/update', async function(req, res) {
+
+    console.log(reg.body)
+
+    const { sms_price,
+        call_price,
+        price_plan} = req.body;
+
+       console.log('req.body');
+
+    res.json({
+        status : 'success'
+    })
+});
+
+app.post('/api/phonebill/', async function(req, res) {
+
+    const price_plan = await db.get(`SELECT id, plan_name, sms_price, call_price
+        FROM price_plan where plan_name = ?`, req.body.price_plan);
+
+        const activity = re.body.actions;
+        
+   //totalPhonebill logic
+   const activities = activity.split(",");
+    let total = 0;
+
+    activities.forEach(action => {
+        if (action.trim() === 'sms') {
+            total += price_plan.sms_price;
+        } else if (action.trim() == 'call') {
+            total += price_plan.call_price;
+        }
+    });
+
+    res.json({
+        total
+    })
+
 
 app.get('/api/price_plans', async function(req, res) {
 
