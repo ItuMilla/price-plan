f84b2fd9179dadfeb0ba20acc282a8bcc664969f
ItuMilla <itumelenglephondo94@gmail.com>
2022-09-08 13:53:58 +0200

    git commit -m "price_plan"

--- index.js
+++ index.js
@@ -1,57 +1,28 @@
 import * as sqlite from 'sqlite';
 import sqlite3 from 'sqlite3';
 import express from 'express';
-
 const app = express();
-
-// make re.body work
+//make req.body work
 app.use(express.json());
-
 const db = await sqlite.open({
     filename: './data_plan.db',
     driver: sqlite3.Database
 });
-
-console.log('db initialized');
-
-
-// await db.migrate();  
-
+console.log('db initializeds');
+await db.migrate();
 app.get('/', async function (req, res) {
-    const pricePlans = await db.all(`select * from price_plan`)
+    const fetchAllPlans = await db.all(`select * from price_plan`);
     res.json({
-        price_plans: pricePlans
+        price_plans: fetchAllPlans
     })
 })
-
-app.post('/api/price_plan/update', async function (req, res) {
-    const { name, call_cost, sms_cost } = req.body;
-    await db.all(`update price_plan set plan_name = ?, sms_price = ?, call_price = ? where plan_name = ?`,
-    name,sms_cost,call_cost,name)
-    console.log(req.body)
-    // res.json({
-    //     status: 'success'
-    // })
-
-    // const someData = {
-    //     sms_price,
-    //     call_price,
-    //     // price_plan} = req.body;
-    // }
-
-});
-
-app.post('/api/phonebill/', async function (req, res) {
-
-    const price_plan = await db.get(`SELECT id, plan_name, sms_price, call_price 
+app.post(`/api/phonebill/`, async function (req, res) {
+    const price_plan = await db.get(`SELECT id, plan_name, sms_price, call_price
     FROM price_plan where plan_name = ?`, req.body.price_plan);
-
     const activity = req.body.actions;
-
-    //totalPhonebill logic
+    //totalPhoneBill logic
     const activities = activity.split(",");
     let total = 0;
-
     activities.forEach(action => {
         if (action.trim() === 'sms') {
             total += price_plan.sms_price;
@@ -59,39 +30,152 @@ app.post('/api/phonebill/', async function (req, res) {
             total += price_plan.call_price;
         }
     });
-
     res.json({
         total
     })
-})
-
-
-app.get('/api/price_plans', async function (req, res) {
-
-    const price_plans = await db.all('select * from price_plan');
-
+});
+app.get(`/api/price_plans`, async function (req, res) {
+    const price_plans = await db.all(`select * from price_plan`)
     res.json({
         price_plans
     })
-
 });
-
+app.post('/api/price_plan/update', async function (req, res) {
+    console.log(req.body);
+    const { price_plan, sms_price, call_price } = req.body;
+    await db.all(`update price_plan set sms_price = ?, call_price = ? where plan_name = ?`, sms_price, call_price, price_plan)
+});
 app.post('/api/price_plan/delete', async function (req, res) {
     const { plan_name } = req.body;
     await db.all(`delete from price_plan where plan_name = ?`, plan_name);
-});
-
+})
 app.post('/api/price_plan/add', async function (req, res) {
     const { plan_name, sms_price, call_price } = req.body;
-
     await db.all(`insert into price_plan (plan_name, sms_price, call_price) values (?, ?, ?)`,
         plan_name, sms_price, call_price)
 })
-
-console.log("done!")
-
-
-const PORT = process.env.PORT || 6001;
+console.log("done!");
+const PORT = 6001;
+app.listen(PORT, function () {
+    console.log(`Price plan API started on port ${PORT}`)
+});import * as sqlite from 'sqlite';
+import sqlite3 from 'sqlite3';
+import express from 'express';
+const app = express();
+//make req.body work
+app.use(express.json());
+const db = await sqlite.open({
+    filename: './data_plan.db',
+    driver: sqlite3.Database
+});
+console.log('db initializeds');
+await db.migrate();
+app.get('/', async function (req, res) {
+    const fetchAllPlans = await db.all(`select * from price_plan`);
+    res.json({
+        price_plans: fetchAllPlans
+    })
+})
+app.post(`/api/phonebill/`, async function (req, res) {
+    const price_plan = await db.get(`SELECT id, plan_name, sms_price, call_price
+    FROM price_plan where plan_name = ?`, req.body.price_plan);
+    const activity = req.body.actions;
+    //totalPhoneBill logic
+    const activities = activity.split(",");
+    let total = 0;
+    activities.forEach(action => {
+        if (action.trim() === 'sms') {
+            total += price_plan.sms_price;
+        } else if (action.trim() == 'call') {
+            total += price_plan.call_price;
+        }
+    });
+    res.json({
+        total
+    })
+});
+app.get(`/api/price_plans`, async function (req, res) {
+    const price_plans = await db.all(`select * from price_plan`)
+    res.json({
+        price_plans
+    })
+});
+app.post('/api/price_plan/update', async function (req, res) {
+    console.log(req.body);
+    const { price_plan, sms_price, call_price } = req.body;
+    await db.all(`update price_plan set sms_price = ?, call_price = ? where plan_name = ?`, sms_price, call_price, price_plan)
+});
+app.post('/api/price_plan/delete', async function (req, res) {
+    const { plan_name } = req.body;
+    await db.all(`delete from price_plan where plan_name = ?`, plan_name);
+})
+app.post('/api/price_plan/add', async function (req, res) {
+    const { plan_name, sms_price, call_price } = req.body;
+    await db.all(`insert into price_plan (plan_name, sms_price, call_price) values (?, ?, ?)`,
+        plan_name, sms_price, call_price)
+})
+console.log("done!");
+const PORT = 6001;
+app.listen(PORT, function () {
+    console.log(`Price plan API started on port ${PORT}`)
+});import * as sqlite from 'sqlite';
+import sqlite3 from 'sqlite3';
+import express from 'express';
+const app = express();
+//make req.body work
+app.use(express.json());
+const db = await sqlite.open({
+    filename: './data_plan.db',
+    driver: sqlite3.Database
+});
+console.log('db initializeds');
+await db.migrate();
+app.get('/', async function (req, res) {
+    const fetchAllPlans = await db.all(`select * from price_plan`);
+    res.json({
+        price_plans: fetchAllPlans
+    })
+})
+app.post(`/api/phonebill/`, async function (req, res) {
+    const price_plan = await db.get(`SELECT id, plan_name, sms_price, call_price
+    FROM price_plan where plan_name = ?`, req.body.price_plan);
+    const activity = req.body.actions;
+    //totalPhoneBill logic
+    const activities = activity.split(",");
+    let total = 0;
+    activities.forEach(action => {
+        if (action.trim() === 'sms') {
+            total += price_plan.sms_price;
+        } else if (action.trim() == 'call') {
+            total += price_plan.call_price;
+        }
+    });
+    res.json({
+        total
+    })
+});
+app.get(`/api/price_plans`, async function (req, res) {
+    const price_plans = await db.all(`select * from price_plan`)
+    res.json({
+        price_plans
+    })
+});
+app.post('/api/price_plan/update', async function (req, res) {
+    console.log(req.body);
+    const { price_plan, sms_price, call_price } = req.body;
+    await db.all(`update price_plan set sms_price = ?, call_price = ? where plan_name = ?`, sms_price, call_price, price_plan)
+});
+app.post('/api/price_plan/delete', async function (req, res) {
+    const { plan_name } = req.body;
+    await db.all(`delete from price_plan where plan_name = ?`, plan_name);
+})
+app.post('/api/price_plan/add', async function (req, res) {
+    const { plan_name, sms_price, call_price } = req.body;
+    await db.all(`insert into price_plan (plan_name, sms_price, call_price) values (?, ?, ?)`,
+        plan_name, sms_price, call_price)
+})
+console.log("done!");
+const PORT = 6001;
 app.listen(PORT, function () {
     console.log(`Price plan API started on port ${PORT}`)
 });
